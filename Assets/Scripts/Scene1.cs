a3e9c19fce23839b1aceac0c79144455c765a65c
Kenndly <65460232+Kenndly@users.noreply.github.com>
2020-06-27 15:59:58 +0500

    С интрукцией

---
+++ Assets/Scripts/Scene1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene1 : MonoBehaviour
+{
+    public GameObject Instruct, StartP, LoadScreen;
+    // Start is called before the first frame update
+    public void InstructPanel()
+    {
+        StartP.SetActive(false);
+        Instruct.SetActive(true);
+    }
+    public void Scene2()
+    {
+        Instruct.SetActive(false);
+        LoadScreen.SetActive(true);
+        Application.LoadLevel(1);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
