2e57636c666affe264e51bbe78e2725d7952f329
Nikita Romanov <system.program.code@gmail.com>
2020-07-17 21:04:38 +0500

    Еще один коммит

---
+++ Assets/Scripts/JumpTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTrigger : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("1");
+        animator.Play("Jump");
+    }
+}
