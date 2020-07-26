8533c3b1a095e4ecbf7f669b810b8e85dc26df05
Nikita Romanov <system.program.code@gmail.com>
2020-07-26 22:51:30 +0500

    Много чего сделал

---
+++ Assets/Scripts/LoadARbutton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadARbutton : MonoBehaviour
+{
+    public GameObject AR_Camera;
+    public GameObject Game_Camera;
+    public GameObject Canvases;
+    public GameObject OtherCanvases;
+
+    public void AR()
+    {
+        AR_Camera.SetActive(true);
+        Canvases.SetActive(false);
+        OtherCanvases.SetActive(true);
+        Game_Camera.SetActive(false);
+    }
+
+    public void Game()
+    {
+        Game_Camera.SetActive(true);
+        Canvases.SetActive(true);
+        OtherCanvases.SetActive(false);
+        AR_Camera.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/YetAnotherARscript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YetAnotherARscript : MonoBehaviour
+{
+    private bool isOn = false;
+    public GameObject Canvas;
+    public GameObject ImageTarget;
+
+    private void Update()
+    {
+        if (ImageTarget.activeSelf && !isOn)
+        {
+            isOn = true;
+            Canvas.SetActive(false);
+        }
+
+        if (!ImageTarget.activeSelf && isOn)
+        {
+            isOn = false;
+            Canvas.SetActive(true);
+        }
+    }
+}
